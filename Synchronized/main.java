6374966ff4769ac1cb274db0b15bfd4830d77aab
gianfrancorecalde <francorecalde19998@gmail.com>
2021-09-18 13:29:01 -0300

    sigo con tp3

--- Synchronized/main.java
+++ Synchronized/main.java
@@ -10,11 +10,30 @@ public class main {
 
     } */
 
-    public static void main(String[] args) {
+    /* public static void main(String[] args) {
         Vida v = new Vida(10);
         Thread orco = new Thread(new Daniar(v), "Orco");
         Thread curandero = new Thread(new Curar(v), "Curandero");
         orco.start();
         curandero.start();
+    } */
+
+    
+    
+    /*      EJERCICIO 4              */
+
+
+    public static void main(String[] args) {
+        
+        GestorActividad gestorAct = new GestorActividad();
+        Thread hamster1 = new Thread(gestorAct, "hasmter1");
+        Thread hamster2 = new Thread(gestorAct, "hasmter2");
+        Thread hamster3 = new Thread(gestorAct, "hasmter3");
+
+        hamster1.start();
+        hamster2.start();
+        hamster3.start();
+        
     }
+
 }
---
+++ UtilidadesLeoBustamante/Colores.java
@@ -0,0 +1,62 @@
+package ProgrmacionConcurrente.UtilidadesLeoBustamante;
+
+public class Colores {
+    public static final String NEGRO = "\u001B[30m";
+    public static final String ROJO = "\u001B[31m";
+    public static final String VERDE = "\u001B[32m";
+    public static final String AMARILLO = "\u001B[33m";
+    public static final String AZUL = "\u001B[34m";
+    public static final String PURPLE = "\u001B[35m";
+    public static final String CYAN = "\u001B[36m";
+    public static final String BLANCO = "\u001B[37m";
+    public static final String RESET = "\u001B[0m";
+    private String unColor;
+
+    public Colores(String unColor) {
+        switch (unColor) {
+            case "rojo":
+                this.unColor = ROJO;
+                break;
+            case "verde":
+                this.unColor = VERDE;
+                break;
+            case "amarillo":
+                this.unColor = AMARILLO;
+                break;
+            case "azul":
+                this.unColor = AZUL;
+                break;
+            case "violeta":
+                this.unColor = PURPLE;
+                break;
+            case "cyan":
+                this.unColor = CYAN;
+                break;
+            case "blanco":
+                this.unColor = BLANCO;
+                break;
+            case "negro":
+                this.unColor = NEGRO;
+                break;
+            case "aleatorio":
+                String array[] = {ROJO, VERDE, AMARILLO, AZUL, PURPLE, CYAN, BLANCO, NEGRO};
+                int number = (int) (Math.random() * 7);
+                this.unColor = array[number];
+                break;
+            default:
+                this.unColor = NEGRO;
+                break;
+        }
+
+    }
+
+    public String pintar(String aCadena) {
+        String aux = this.unColor;
+        return aux + aCadena + RESET;
+    }
+
+    public static String colorAleatorio(String texto) {
+        Colores q = new Colores("aleatorio");
+        return q.pintar(texto);
+    }
+}
